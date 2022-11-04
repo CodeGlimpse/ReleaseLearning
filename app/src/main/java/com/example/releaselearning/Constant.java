2a60694060513d1c422fb949e78f07fc1c6e5413
Binturo <919775428@qq.com>
2022-11-04 17:13:16 +0800

    改动了网址

--- app/src/main/java/com/example/releaselearning/Constant.java
+++ app/src/main/java/com/example/releaselearning/Constant.java
@@ -2,8 +2,8 @@ package com.example.releaselearning;
 
 public class Constant {
 
-    public static final String URLQueryIdAndPassword = "http://192.168.43.71:8080/login_stu";
-    public static final String URLAdd = "http://192.168.43.71:8080/register_stu";
+    public static final String URLQueryIdAndPassword = "http://10.7.86.170:8080/androidStudent/login_stu";
+    public static final String URLAdd = "http://10.7.86.170:8080/androidStudent/register_stu";
     public static final int SUCCEED_CODE = 1;
     public static final int UNSUCCEED_CODE = 2;
 }
